2835e7a4d25693755a59e6692166a971ffcf860d
Evan Smith <12199891+e13h@users.noreply.github.com>
2023-07-23 13:56:13 -0700

    Add dynamic contact info. Resolve linting issues. Add parents and default nickname. Introduce networkx graph.

--- birthday_calendar.py
+++ birthday_calendar.py
@@ -1,34 +1,42 @@
 import json
 from datetime import datetime
-from attrs import define, field
+from textwrap import indent
 
-from ics import Calendar, Event
-import phonenumbers
 import networkx as nx
+import phonenumbers
+from attrs import define, field
+from ics import Calendar, Event
 
 
-def normalize_phone_number(input: str, region: str = "US") -> str:
-    if input is None:
+def normalize_phone_number(phone: str, region: str = "US") -> str:
+    if phone is None:
         return None
     return phonenumbers.format_number(
-        phonenumbers.parse(input, region),
-        phonenumbers.PhoneNumberFormat.NATIONAL)
+        phonenumbers.parse(phone, region), phonenumbers.PhoneNumberFormat.NATIONAL
+    )
 
-def convert_date(input: str) -> datetime | None:
-    if input is None:
+
+def convert_date(date: str) -> datetime | None:
+    if date is None:
         return None
-    return datetime.strptime(input, "%Y-%m-%d")
+    return datetime.strptime(date, "%Y-%m-%d")
 
 
 @define(frozen=True)
 class Person:
+    id: str
     first_name: str
     last_name: str
     birthday: datetime = field(converter=convert_date)
     anniversary: datetime = field(default=None, converter=convert_date)
-    phone: str = field(default=None, converter=lambda phone: normalize_phone_number(phone))
+    phone: str = field(default=None, converter=normalize_phone_number)
     email: str = None
-    nickname: str = None
+    nickname: str = field()
+    parents: tuple[str] = field(default=tuple())
+
+    @nickname.default
+    def _first_name_is_nickname(self) -> str:
+        return self.first_name
 
 
 def ordinal(n: int):
@@ -39,27 +47,40 @@ def ordinal(n: int):
     return str(n) + suffix
 
 
-def load_from_file(filepath: str) -> list[Person]:
+def load_from_file(filepath: str) -> dict[str, Person]:
     with open(filepath, "rb") as file:
         people = json.load(file)
-    return [Person(**person) for person in people]
+    assert isinstance(people, list)
+    return {person["id"]: Person(**person) for person in people}
+
+
+def create_contact_info(person: Person) -> str:
+    phone_contact = f"\n📱 {person.phone}" if person.phone else ""
+    email_contact = f"\n📧 {person.email}" if person.email else ""
+    if phone_contact or email_contact:
+        contact_info = f"{person.nickname}:{phone_contact}{email_contact}"
+        contact_info = "\n\nContact info:\n" + indent(contact_info, "    ")
+        return contact_info
+    return ""
 
 
 def generate_birthday_events(person: Person, num_years: int = 110) -> list[Event]:
     events = []
     year = person.birthday.year
-    description = f"""
-{person.first_name} {person.last_name} was born on {person.birthday.strftime("%B %d, %Y")}.
-
-Contact info:
-    {person.nickname if person.nickname else person.first_name}:
-    📱 {person.phone}
-    📧 {person.email}
-    """.strip()
+    contact_info = create_contact_info(person)
+    if contact_info == "" and len(person.parents) > 0:
+        contact_info = "\n".join(
+            [create_contact_info(parent) for parent in person.parents]
+        )
+    bday_formatted = person.birthday.strftime("%B %d, %Y")
+    description = (
+        f"{person.first_name} {person.last_name} was born on {bday_formatted}."
+        + contact_info
+    )
     print(description)
     for i in range(num_years):
         tag = f"{ordinal(i)} " if i > 0 else ""
-        title = f"{person.nickname if person.nickname else person.first_name}'s {tag}Birthday"
+        title = f"{person.nickname}'s {tag}Birthday"
         date = f"{year + i}-{person.birthday.month}-{person.birthday.day}"
         e = Event(name=title, begin=date, description=description)
         e.make_all_day()
@@ -69,11 +90,11 @@ Contact info:
 
 def main():
     people = load_from_file("small_family.json")
-    graph = nx.Graph()
-    graph.add_nodes_from(people)
+    graph = nx.DiGraph()
+    graph.add_nodes_from(people.values())
     print(people)
     events = []
-    for person in people:
+    for person in people.values():
         events.extend(generate_birthday_events(person))
     cal = Calendar(events=events)
     with open("test.ics", "w", encoding="utf-8") as f:
