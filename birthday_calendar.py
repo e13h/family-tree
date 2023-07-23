fa9413ef6bd33f0693a3dbaf0bd58cd18246abd5
Evan Smith <12199891+e13h@users.noreply.github.com>
2023-07-23 13:56:13 -0700

    Format

--- birthday_calendar.py
+++ birthday_calendar.py
@@ -1,7 +1,8 @@
 import json
-from ics import Calendar, Event
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime
+
+from ics import Calendar, Event
 
 
 @dataclass
@@ -11,18 +12,22 @@ class Person:
     birthday: datetime
     anniversary: datetime = None
 
-    def __init__(self, first_name: str, last_name: str, birthday: str, anniversary: str = None):
+    def __init__(
+        self, first_name: str, last_name: str, birthday: str, anniversary: str = None
+    ):
         self.first_name = first_name
         self.last_name = last_name
         self.birthday = datetime.strptime(birthday, "%Y-%m-%d")
-        self.anniversary = datetime.strptime(anniversary, "%Y-%m-%d") if anniversary else None
+        self.anniversary = (
+            datetime.strptime(anniversary, "%Y-%m-%d") if anniversary else None
+        )
 
 
 def ordinal(n: int):
     if 11 <= (n % 100) <= 13:
-        suffix = 'th'
+        suffix = "th"
     else:
-        suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
     return str(n) + suffix
 
 
@@ -30,6 +35,7 @@ def load_from_file(filepath: str) -> list[Person]:
     with open(filepath, "rb") as file:
         people = json.load(file)
     return [Person(**person) for person in people]
+
 
 def generate_birthday_events(person: Person, num_years: int = 110) -> list[Event]:
     events = []
@@ -53,6 +59,7 @@ def main():
     cal = Calendar(events=events)
     with open("test.ics", "w", encoding="utf-8") as f:
         f.writelines(cal.serialize_iter())
+
 
 if __name__ == "__main__":
     main()
